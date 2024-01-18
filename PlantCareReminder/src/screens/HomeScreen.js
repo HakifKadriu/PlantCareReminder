a723905bf42ac69dd1ab10ff1fe780ee0f332489
Kifaa <hakifkadriu2003@gmail.com>
2024-01-18 02:11:13 +0100

    testing

--- PlantCareReminder/src/screens/HomeScreen.js
+++ PlantCareReminder/src/screens/HomeScreen.js
@@ -4,7 +4,6 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function HomeScreen() {
     return (
 <View style={styles.container}>
-    <Text>This is Home Screen!</Text>
 </View>
     )
 }
