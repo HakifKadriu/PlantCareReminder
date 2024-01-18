24c9e6b3fb686743171a3006b58fe554da76dbe7
flamurisa <flamurisa21@gmail.com>
2024-01-18 02:37:06 +0100

    hajde ma dil nat qim nane

--- PlantCareReminder/src/screens/Notifications.js
+++ PlantCareReminder/src/screens/Notifications.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function Notifications() {
     return (
 <View style={styles.container}>
-    <Text>This is Notifications screen! sdadasda
+    <Text>This is Notifications screen! QITASH PERNIME E KOM PASHA NI ALLAG
         dasdasd
     </Text>
 </View>
