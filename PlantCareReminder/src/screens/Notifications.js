ea1a0c774884eb6d0eb70db2c55440aa79f61e19
flamurisa <flamurisa21@gmail.com>
2024-01-22 13:46:46 +0100

    test
    veq test

--- PlantCareReminder/src/screens/Notifications.js
+++ PlantCareReminder/src/screens/Notifications.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 export default function Notifications() {
     return (
-<View style={styles.container}>
-    <Text>This is Notifications screen! QITASH PERNIME E KOM PASHA NI ALLAG
-        dasdasd
-    </Text>
+<View style={styles.AllNotifications}>
+    <Button tittle= "All Notifications" color="green"/>
+ 
 </View>
     )
 }
